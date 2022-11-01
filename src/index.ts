26f0c85c26f007acd93067ca4d613399f2180119
PreciseAardvark <abraha.delarosaaguayo.509@gmail.com>
2022-11-01 10:20:14 -0600

    feature: use dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+require('dotenv').config()
 import express from "express";
-import router from './routes'
+import router from './routes';
 
+const port = process.env.port
 
 export const app = express();
-
 app.use(express.json());
 app.use('/tacos', router)
 app.get('/', (req, res) =>{
   res.json("Servidor online :D")
 })
-app.listen(3000);
+app.listen(port);
